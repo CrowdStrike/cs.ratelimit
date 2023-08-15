bdf73b4e6c58636fc683ca378c568cbf7a74bd35
Forrest Aldridge <forrest.aldridge@crowdstrike.com>
2023-08-14 21:32:10 -0500

    Add GitHub actions.

--- util/lint.sh
+++ util/lint.sh
@@ -2,3 +2,4 @@
 
 flake8 "cs/ratelimit" --count --exit-zero --max-complexity=15 --max-line-length=127 --statistics
 pylint "cs/ratelimit" --exit-zero --max-line-length=127 --disable=R0801 --ignore=tests
+pydocstyle "cs/ratelimit" --count
